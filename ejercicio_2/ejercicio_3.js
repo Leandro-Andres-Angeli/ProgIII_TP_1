e9ec2926ea63bbbb7e2c302c5a1d99c167336871
= <=>
2024-08-19 23:05:00 -0300

    msj added

--- ejercicio_2/ejercicio_3.js
+++ ejercicio_2/ejercicio_3.js
@@ -2,32 +2,31 @@
 
 const urlApi = 'https://fakestoreapi.com/products';
 
-async function agregarProducto(objeto){
-    try{
-        const response = await fetch(urlApi, { 
-             method:"POST",
-             body:JSON.stringify(objeto)
-        });
-        if(!response.ok){
-            throw new Error('error', response.status);
-        }
+async function agregarProducto(objeto) {
+  try {
+    const response = await fetch(urlApi, {
+      method: 'POST',
+      body: JSON.stringify(objeto),
+    });
+    if (!response.ok) {
+      throw new Error('error', response.status);
+    }
 
-        const datos = await response.json();
-        console.log(datos);
-   
-        }catch(error){
-            console.log(error);
-        }
-
+    const datos = await response.json();
+    console.log('producto agregado', datos);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const  productoNuevo= {
-    id: 21,
-    title: 'Camperas de abrigo',
-    price: 250.50,
-    description: '70%Cotton,10%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
-    category: "Ropa de mujer",
-    image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
-    rating: { rate: 3.6, count: 145 }
-}
+const productoNuevo = {
+  id: 21,
+  title: 'Camperas de abrigo',
+  price: 250.5,
+  description:
+    '70%Cotton,10%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
+  category: 'Ropa de mujer',
+  image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
+  rating: { rate: 3.6, count: 145 },
+};
 agregarProducto(productoNuevo);
