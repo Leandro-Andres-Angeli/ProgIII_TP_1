e110294224f8543864318b6f9b4cc9ae69508c7c
= <=>
2024-08-24 19:47:42 -0300

    fixed error handling

--- ejercicio_2/ejercicio_4.js
+++ ejercicio_2/ejercicio_4.js
@@ -6,14 +6,14 @@ async function agregarProducto(id) {
   try {
     const response = await fetch(urlApi + '/' + id);
 
-    if (!response.ok) {
-      throw new Error('error', response.status);
+    if (response.status !== 200) {
+      throw new Error(`Error ${response.status} ${response.statusText}`);
     }
 
     const datos = await response.json();
     console.log(datos);
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
   }
 }
 agregarProducto(5);
